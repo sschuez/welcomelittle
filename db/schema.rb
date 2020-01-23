7c040fe709ab00de149bfbfa8c863793a212bd03
Stephen Schuez <stephen.schuez@gmail.com>
2020-01-23 14:34:51 +0100

    Add Model Inquiry to App

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_122133) do
+ActiveRecord::Schema.define(version: 2020_01_23_133215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "email"
+    t.string "newspaper"
+    t.string "other_newspaper"
+    t.date "desired_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
