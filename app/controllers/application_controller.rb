8887ce5622f2972de7aa008635954a2280994f43
Stephen Schuez <stephen.schuez@gmail.com>
2020-02-06 15:46:30 +0100

    googel analytics, take 2, plus minor changes in favicons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
-	before_action :add_www_subdomain
+	# before_action :add_www_subdomain
 
 	# app/controllers/application_controller.rb
 	def default_url_options
@@ -9,11 +9,11 @@ class ApplicationController < ActionController::Base
 	
 	private
 	
-  def add_www_subdomain
-    unless /^www/.match(request.host)
-      redirect_to("#{request.protocol}x.com#{request.request_uri}",
-                  :status => 301)
-    end
-  end
+  # def add_www_subdomain
+  #   unless /^www/.match(request.host)
+  #     redirect_to("#{request.protocol}x.com#{request.request_uri}",
+  #                 :status => 301)
+  #   end
+  # end
 
 end
