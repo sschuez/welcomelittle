fc42fe688b29084ffdce4febea519cf47f9b05d3
Stephen Schuez <stephen.schuez@gmail.com>
2020-02-06 14:14:30 +0100

    remove sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,4 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/sitemap.xml' => 'sitemaps#index', defaults: { format: 'xml' }
 end
