5f1a0c2694968d3bfe0b1267e8e983ff2b99ac4d
Stephen Schuez <stephen.schuez@gmail.com>
2020-02-07 20:19:22 +0100

    first installation of postmark

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,4 +10,13 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Wilkommen bei in-frieden.ch')
     # This will render a view in `app/views/user_mailer`!
   end
+
+  def hello
+    mail(
+      :subject => 'Hello from Postmark',
+      :to  => 'stephen.schuez@gmail.com',
+      :from => 'info@in-frieden.ch',
+      :html_body => '<strong>Hello</strong> dear Postmark user.',
+      :track_opens => 'true')
+  end
 end
