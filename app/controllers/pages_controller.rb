383efcf335601a58545272a9b788a9189d3a7531
Stephen Schuez <stephen.schuez@gmail.com>
2020-05-06 18:12:10 +0200

    Changes styling and created new content for landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:home, :articles, :contact]
 	
   def home
+  	@articles = Article.all
   end
 
   def contact
