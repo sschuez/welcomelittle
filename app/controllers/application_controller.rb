1df9e02b564e5fbc3be0aba3851533ef4d9eaaa2
Stephen Schuez <stephen.schuez@gmail.com>
2020-05-14 11:15:16 +0200

    added first and last name to devise and to appl controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit
 
@@ -23,6 +24,13 @@ class ApplicationController < ActionController::Base
 	  
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name]) 
   end
 
 end
